d86e64f2d6826bbd9b5b549692e0e019775a0e46
dev-slidingapps <slidingapps@gmail.com>
2016-11-17 15:33:21 +0100

    jquery + autofocus

--- src/app/component/account/button.component.ts
+++ src/app/component/account/button.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -9,17 +9,23 @@ import { FormGroup } from '@angular/forms';
     <div class="row">
         <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-lg-4 col-lg-offset-4">
             <div [formGroup]="formGroup" class="form-group">
-                <button type="submit" class="btn btn-orange submit" [disabled]="!formGroup.valid">{{ text }}</button>
+                <button type="submit" class="btn btn-orange submit" [disabled]="!formGroup.valid" disabled>{{ text }}</button>
             </div>
         </div>
     </div>
     <!-- COMPONENT.ACCOUNT.BUTTON: END -->
     `
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
     @Input()
     public formGroup: FormGroup;
 
     @Input()
     public text: string;
+
+    public isInitialized: boolean = false;
+
+    public ngOnInit(): void {
+        this.isInitialized = true;
+    }
 }
